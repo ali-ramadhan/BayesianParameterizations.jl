8030ef6602d819258fed0c5bf980aee11b4c607e
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2022-03-10 21:57:25 -0500

    Got some inference going I think

--- bayesian_advection_1d.jl
+++ bayesian_advection_1d.jl
@@ -1,6 +1,17 @@
 using OffsetArrays
 using DifferentialEquations
+using Turing
 using CairoMakie
+
+# Turing.setadbackend(:forwarddiff)
+
+# Exact solution
+
+## Convert x to periodic range in [0, L].
+@inline x′(x, t, L, U) = mod(x + L - U * t, L)
+@inline ϕ_exact(x, t, L, U) = exp(-100 * (x′(x, t, L, U) - 0.5)^2)
+
+# Numerical solution
 
 @inline advective_flux(i, u, ϕ) = u * (ϕ[i-1] + ϕ[i]) / 2
 
@@ -20,7 +31,7 @@ function advection(∂ϕ∂t, ϕ, p, t)
     end
 end
 
-Nx = 256
+Nx = 64
 Hx = 1
 Lx = 1
 Δx = Lx / Nx
@@ -31,20 +42,22 @@ xC = range(Δx/2, Lx - Δx/2, length=Nx)
 xF = range(0, Lx, length=Nx+1)
 
 ϕ₀ = OffsetArray(zeros(Nx+2Hx), -Hx+1:Nx+Hx)
-@. ϕ₀[1:Nx] = exp(-100*(xC-0.5)^2)
+@. ϕ₀[1:Nx] = ϕ_exact.(xC, 0, Lx, u)
 
 tspan = (0.0, 10.0)
 prob = ODEProblem(advection, ϕ₀, tspan, p, saveat=0.1)
 sol = solve(prob, Tsit5())
 
-function animate_advection(ϕ, x, t; framerate=30)
+function animate_advection(ϕ, ϕ_correct, x, t; framerate=30)
     n = Observable(1)
     ϕₙ = @lift ϕ[:, $n]
+    ϕₙ_correct = @lift ϕ_correct[:, $n]
 
     title = @lift "Advection: t = $(round(t[$n], digits=1))"
 
     fig = Figure()
     ax = Axis(fig[1, 1], title=title, xlabel="x", ylabel="ϕ")
+    lines!(ax, x, ϕₙ_correct)
     lines!(ax, x, ϕₙ)
 
     time_indices = 1:length(t)
@@ -55,4 +68,34 @@ function animate_advection(ϕ, x, t; framerate=30)
 end
 
 ϕ = Array(sol)[Hx+1:Nx+Hx, :]
-animate_advection(ϕ, xC, sol.t)
+ϕ_correct = ϕ_exact.(xC, sol.t', Lx, u)
+animate_advection(ϕ, ϕ_correct, xC, sol.t)
+
+@model function probabilistic_advection(data, prob)
+    Nx, Hx, Δx, _ = prob.p
+    Nx = Int(Nx)
+    Hx = Int(Hx)
+
+    σ ~ Uniform(0, 0.01)
+    u ~ Uniform(0.18, 0.22)
+
+    p = [Nx, Hx, Δx, u]
+    prob = remake(prob, p=p)
+    prediction = solve(prob, Tsit5())
+
+    for i = 1:length(prediction)
+        data[:, i] ~ MvNormal(prediction[i][1:Nx], σ)
+    end
+end
+
+stat_model = probabilistic_advection(ϕ_correct, prob)
+
+function callback(rng, model, sampler, sample, state, iter; kwargs...)
+    @show iter
+end
+
+ϵ, τ = 1e-2, 100
+n_samples = 100
+chain = sample(stat_model, NUTS(0.65), n_samples, progress=true)
+
+# chain = sample(stat_model, HMC(0.01, 10), MCMCThreads(), 1000, 3; callback)
