23b77dee19deea02c4a8d17ed1c231b8ba076e05
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2022-03-13 19:11:55 -0400

    Finally figured out the issue

--- bayesian_advection_1d.jl
+++ bayesian_advection_1d.jl
@@ -6,19 +6,17 @@ using DifferentialEquations
 using Turing
 using DynamicHMC
 using CairoMakie
+using ElectronDisplay
 
 # Turing.setadbackend(:forwarddiff)
 
 # Exact solution
-
-## Convert x to periodic range in [0, L].
+# Convert x to periodic range in [0, L].
 @inline x′(x, t, L, U) = mod(x + L - U * t, L)
 @inline ϕ_exact(x, t, L, U) = exp(-100 * (x′(x, t, L, U) - 0.5)^2)
 
 # Numerical solution
-
 @inline advective_flux(i, u, ϕ) = u * (ϕ[i-1] + ϕ[i]) / 2
-
 @inline ∂x_advective_flux(i, Δx, u, ϕ) = (advective_flux(i+1, u, ϕ) - advective_flux(i, u, ϕ)) / Δx
 
 function advection(∂ϕ∂t, ϕ, p, t)
@@ -73,7 +71,6 @@ end
 
 ϕ = Array(sol)[Hx+1:Nx+Hx, :]
 ϕ_correct = ϕ_exact.(xC, sol.t', Lx, u)
-# ϕ_correct = @. ϕ_correct + 1e-2 * randn()
 animate_advection(ϕ, ϕ_correct, xC, sol.t)
 
 function predict_advection(prob, u)
@@ -88,9 +85,45 @@ function predict_advection(prob, u)
     return prediction
 end
 
+function plot_likelihood(prob; saveat=0.1, N=1001, u_range=(-1, 1), chains=nothing)
+    prob = remake(prob; saveat)
+    Nx, Hx, _, u = prob.p
+    Nx = Int(Nx)
+    Hx = Int(Hx)
+
+    losses = zeros(N)
+    us = range(u_range[1], u_range[2], length=N)
+
+    for (i, ũ) in enumerate(us)
+        prediction = predict_advection(prob, ũ)
+        ϕ_correct = ϕ_exact.(xC, prediction.t', Lx, u)
+        prediction_array = Array(prediction)[1+Hx:Nx+Hx, :]
+        losses[i] = mean((prediction_array .- ϕ_correct).^2)
+    end
+
+    fig = Figure()
+    ax = Axis(fig[1, 1], title="Advection 1D with saveat=$saveat", xlabel="u", ylabel="likelihood ~ -loss function")
+    lines!(ax, us, maximum(losses) .- losses)
+
+    if !isnothing(chains)
+        n_chains = size(chains, 3)
+        for c in 1:n_chains
+            vlines!(ax, mode(chains[:u].data[:, c]), color=(:red, 0.5))
+        end
+    end
+
+    save("advection_1d_likelihood_saveat$saveat.png", fig)
+
+    return
+end
+
+for sa in [0.01, 0.1, 0.2, 0.5, 1.0]
+    plot_likelihood(prob, saveat=sa)
+end
+
 @model function probabilistic_advection(data, prob)
-    σ ~ truncated(Exponential(0.05), 0, 0.1)
-    u ~ truncated(Normal(0.2, 0.05), 0, 0.5)
+    σ ~ InverseGamma(3, 0.5)
+    u ~ Normal(0.2, 0.05)
 
     prediction = predict_advection(prob, u)
 
@@ -99,9 +132,9 @@ end
     end
 end
 
-# prior_chain  = sample(probabilistic_advection(ϕ_correct, prob), Prior(), 1000)
+# prior_chain = sample(probabilistic_advection(ϕ_correct, prob), Prior(), 1000)
 
-function callback(rng, model, sampler, sample, state, iter; loss_history, kwargs...)
+function callback_single_chain(rng, model, sampler, sample, state, iter; loss_history, kwargs...)
     u = sample.θ.u[1][1]
     σ = sample.θ.σ[1][1]
 
@@ -111,22 +144,17 @@ function callback(rng, model, sampler, sample, state, iter; loss_history, kwargs
     @info "Iteration $iter: u=$u, σ=$σ, ℒ=$loss"
 end
 
-stat_model = probabilistic_advection(ϕ_correct, prob)
-
 # sampler = MH(:u => x -> Normal(x, 0.05), :σ => x -> Normal(x, 5e-3))
-sampler = HMC(1e-3, 512)
-# sampler = NUTS(0.65, max_depth=10)
-# sampler = DynamicNUTS()
-# sampler = HMCDA(10, 0.65, 0.3)
+# sampler = HMC(1e-3, 10)
+sampler = NUTS(100, 0.65)
 
-θ₀ = [0.01, 0.2] # Use with the init_params=θ₀ kwarg.
+θ₀ = [0.05, 0.2] # Use with the init_params=θ₀ kwarg.
 
-n_samples = 10000
-loss_history = zeros(n_samples)
-# chain = sample(stat_model, sampler, n_samples, progress=true; callback, loss_history)
-# chain = sample(stat_model, sampler, n_samples, progress=true, init_params=θ₀; callback, loss_history)
+# n_samples = 10000
+# loss_history = zeros(n_samples)
+# chain = sample(probabilistic_advection(ϕ_correct, prob), sampler, n_samples, progress=true, callback=callback_single_chain; loss_history)
 
-function callback(rng, model, sampler, sample, state, iter; loss_history, kwargs...)
+function callback_multi_chain(rng, model, sampler, sample, state, iter; loss_history, kwargs...)
     n = Threads.threadid()
 
     u = sample.θ.u[1][1]
@@ -139,17 +167,10 @@ function callback(rng, model, sampler, sample, state, iter; loss_history, kwargs
 end
 
 n_chains = 8
+@assert Threads.nthreads() >= n_chains
+
 n_samples = 1000
 loss_history = zeros(n_chains, n_samples)
-chains = sample(stat_model, sampler, MCMCThreads(), n_samples, n_chains, progress=true; callback, loss_history)
+chains = sample(probabilistic_advection(ϕ_correct, prob), sampler, MCMCThreads(), n_samples, n_chains, progress=true, callback=callback_multi_chain; loss_history)
 
-# q = vi(stat_model, ADVI(100, 1000))
-
-# N = 1001
-# losses = zeros(N)
-# us = range(-1, 1, length=N)
-# for (i, ũ) in enumerate(us)
-#     prediction = Array(predict_advection(prob, ũ))[1+Hx:Nx+Hx, :]
-#     losses[i] = mean((prediction .- ϕ_correct).^2)
-# end
-# lines(us, maximum(losses) .- losses, axis=(xlabel="u", ylabel="likelihood ~ -loss function"))
+# q = vi(probabilistic_advection(ϕ_correct, prob), ADVI(100, 1000))
